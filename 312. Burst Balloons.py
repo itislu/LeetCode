2bc7a6b4fd9222f935ed2b451ac9a47b1ac50e92
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2025-06-03 13:28:26 +0200

    feat: Solve 312. Burst Balloons

---
+++ 312. Burst Balloons.py
@@ -0,0 +1,70 @@
+import unittest
+from functools import cache
+from typing import List
+
+
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+        @cache
+        def dp(begin: int, end: int, l_bound: int, r_bound: int) -> int:
+            if begin == end:
+                return 0
+            return max(
+                dp(begin, i, l_bound, nums[i])
+                + l_bound * nums[i] * r_bound
+                + dp(i + 1, end, nums[i], r_bound)
+                for i in range(begin, end)
+            )
+
+        return dp(0, len(nums), 1, 1)
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_example_1(self):
+        nums = [3, 1, 5, 8]
+        expected = 167
+        result = self.solution.maxCoins(nums)
+        self.assertEqual(result, expected)
+
+    def test_example_2(self):
+        nums = [1, 5]
+        expected = 10
+        result = self.solution.maxCoins(nums)
+        self.assertEqual(result, expected)
+
+    def test_single_balloon(self):
+        nums = [5]
+        expected = 5
+        result = self.solution.maxCoins(nums)
+        self.assertEqual(result, expected)
+
+    def test_three_balloons(self):
+        nums = [3, 1, 5]
+        expected = 35
+        result = self.solution.maxCoins(nums)
+        self.assertEqual(result, expected)
+
+    def test_all_zeros(self):
+        nums = [0, 0, 0]
+        expected = 0
+        result = self.solution.maxCoins(nums)
+        self.assertEqual(result, expected)
+
+    def test_with_zeros(self):
+        nums = [2, 0, 3]
+        expected = 9
+        result = self.solution.maxCoins(nums)
+        self.assertEqual(result, expected)
+
+    def test_timeout(self):
+        nums = [3,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,3,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,3,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5,8,1,5]
+        expected = 49774
+        result = self.solution.maxCoins(nums)
+        self.assertEqual(result, expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
