42ad0201240b2c3f3ce904a127dfec76995d320e
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2025-06-03 17:05:22 +0200

    feat: Solve 22. Generate Parentheses

---
+++ 22. Generate Parentheses.py
@@ -0,0 +1,41 @@
+import unittest
+from typing import List
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        dp = {"()"}
+        for _ in range(1, n):
+            dp = {
+                new_s
+                for s in dp
+                for new_s in [s[:i] + "()" + s[i:] for i in range(len(s) + 1)]
+            }
+        return list(dp)
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_example_1(self):
+        n = 3
+        expected = ["((()))", "(()())", "(())()", "()(())", "()()()"]
+        result = self.solution.generateParenthesis(n)
+        self.assertEqual(sorted(result), sorted(expected))
+
+    def test_example_2(self):
+        n = 1
+        expected = ["()"]
+        result = self.solution.generateParenthesis(n)
+        self.assertEqual(result, expected)
+
+    def test_n4(self):
+        n = 4
+        expected = ["(((())))", "((()()))", "((())())", "((()))()", "(()(()))", "(()()())", "(()())()", "(())(())", "(())()()", "()((()))", "()(()())", "()(())()", "()()(())", "()()()()"]
+        result = self.solution.generateParenthesis(n)
+        self.assertEqual(sorted(result), sorted(expected))
+
+
+if __name__ == "__main__":
+    unittest.main()
